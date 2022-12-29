fd1547c296f0bd36026f9c4cfb966641cc55e8b3
Diogo Moura <diogomou@gmail.com>
2022-12-28 21:41:43 -0300

    Nova aula for

---
+++ EstrRepetFORpython.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Todo for em python é um "for each"
+
+# In[ ]:
+
+
+# percorrer uma lista
+lista_produtos = ["faca", "garfo", "panela", "frigideira", "flavorstone"]
+
+
+# In[ ]:
+
+
+# calcular o imposto sobre vários valores
+lista_precos = [10, 10, 200, 50, 300]
+
+
+# In[ ]:
+
+
+# percorrendo um dicionário
+produtos = {
+    "faca": 10,
+    "garfo": 10,
+    "panela": 200,
+    "frigideira": 50,
+    "flavorstone": 300,
+}
+
+
+# ### Range
+
+# In[ ]:
+
+
+
+
+
+# ### Exemplo aplicado - análise de vendas
+# 
+# Vendas no arquivo vendasloja.txt
+
+# In[ ]:
+
+
+
+
